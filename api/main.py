90f02e55065dddd3afee35dfa421313297af704a
amit-077 <amitkamat918@gmail.com>
2024-10-13 14:33:02 +0530

    chanegs

--- api/main.py
+++ api/main.py
@@ -25,7 +25,7 @@ def shortURL(new_url: URL):
     subLink = str(shortuuid.ShortUUID().random(length=8))
     data = collection.insert_one({"sublink": subLink, "redirectTo": originalURL})
     print(data.inserted_id)
-    return {"url": new_url.website_URL}
+    return {"shorturl": "rbly.vercel.app/"+subLink}
 
 @app.get("/getAllUrls")
 def getAllData():
